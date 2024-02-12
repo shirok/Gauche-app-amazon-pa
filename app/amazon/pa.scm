25217e4aa52eff4ca944d43676422c26d6bcb9b8
Shiro Kawai <shiro@acm.org>
2024-02-11 23:19:23 -1000

    Adding the meat (wip)

--- app/amazon/pa.scm
+++ app/amazon/pa.scm
@@ -4,23 +4,28 @@
 
 (define-module app.amazon.pa
   (use file.util)
+  (use rfc.json)
   (use rfc.http)
   (use gauche.record)
+  (use app.amazon.auth)
   (export <amazon-pa-api>
-          make-amazon-pa-api amazon-pa-api?))
+          make-amazon-pa-api amazon-pa-api?
+          amazon-pa-get-items))
 (select-module app.amazon.pa)
 
 (define-record-type <amazon-pa-api> %make-amazon-pa-api amazon-pa-api?
   ;; all slots are private
+  %marketplace
   %partner-tag
   %access-key
-  %secret-key)                          ;thunk
+  %secret-key)      ;thunk to prevent accidentally reveal it
 
 (define (%parse-config config)
   (define (getkey name)
     (or (assq-ref config name)
         (errorf "Value for ~a is missing in config." name)))
-  (values (getkey 'partner-tag)
+  (values (getkey 'marketplace)
+          (getkey 'partner-tag)
           (getkey 'access-key)
           (getkey 'secret-key)))
 
@@ -33,6 +38,37 @@
             :if-does-not-exist #f)
           (error "No configuration provided.  Give an alist to make-amazon-pa-api, \
                   or save it in ~/.gauche-amazon-pa-api.scm."))
-    (receive (partner-tag access-key secret-key) (%parse-config config)
-      (%make-amazon-pa-api partner-tag access-key
+    (receive (marketplace partner-tag access-key secret-key)
+        (%parse-config config)
+      (%make-amazon-pa-api marketplace partner-tag access-key
                            (^ _ secret-key)))))
+
+
+(define (%api-signing-key api)
+  (aws4-signing-key :access-id (~ api'%access-key)
+                    :secret-key ((~ api'%secret-key))
+                    :region "us-west-2"
+                    :service "ProductAdvertisingAPI"))
+
+;; API
+(define (amazon-pa-get-items api item-ids
+                             :optional (resources '()))
+  (assume-type api <amazon-pa-api>)
+  (assume-type item-ids (<List> <string>))
+  (assume-type resources (<List> <string>))
+  (let* ([json (construct-json-string
+                `(("ItemIds" . ,(list->vector item-ids))
+                  ("Resources" . ,(list->vector resources))
+                  ("PartnerTag" . ,(~ api'%partner-tag))
+                  ("PartnerType" . "Associates")
+                  ("Marketplace" . ,(~ api'%marketplace))))]
+         [body (construct-json-string json)]
+         [path "/paapi5/getitems"]
+         [headers '(("content-type" "application/json"))])
+    (http-post "webservices.amazon.co.jp" path body
+               :secure #t
+               :headers (aws4-add-auth-headers (%api-signing-key api)
+                                               'POST
+                                               #"https://webservices.amazon.co.jp/~path"
+                                               headers
+                                               body))))
--- package.scm
+++ package.scm
@@ -5,7 +5,7 @@
 (define-gauche-package "Gauche-app-amazon-pa"
   ;; Repository URL, e.g. github
   ;;  This URL uniquely identifies the package.
-  :repository "http://example.com/Gauche-app-amazon-pa.git"
+  :repository "https://github.com/shirok/Gauche-app-amazon-pa.git"
 
   ;;
   :version "1.0"
@@ -19,7 +19,8 @@
   ;; Example:
   ;;     :require (("Gauche" (>= "0.9.5"))  ; requires Gauche 0.9.5 or later
   ;;               ("Gauche-gl" "0.6"))     ; and Gauche-gl 0.6
-  :require (("Gauche" (>= "0.9.14")))
+  :require (("Gauche" (>= "0.9.14"))
+            ("Gauche-app-amazon-auth" (>= "0.1")))
 
   ;; List of providing modules
   ;; NB: This will be recognized >= Gauche 0.9.7.
@@ -39,7 +40,7 @@
 
   ;; List licenses
   ;; e.g. ("BSD")
-  :licenses ()
+  :licenses ("MIT")
 
   ;; Homepage URL, if any.
   ; :homepage "http://example.com/Gauche-app-amazon-pa/"
